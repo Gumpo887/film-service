73069832d40344e932bf9ad88907d860ec923ef2
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-12-29 00:35:56 +0100

    FEATURE: Adds simple JWT parsing and cleans POM file.

--- src/main/java/io/github/cciglesiasmartinez/microservice_template/infrastructure/security/SecurityConfig.java
+++ src/main/java/io/github/cciglesiasmartinez/microservice_template/infrastructure/security/SecurityConfig.java
@@ -1,20 +1,69 @@
 package io.github.cciglesiasmartinez.microservice_template.infrastructure.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        System.out.println("Callin jwt converter");
+        JwtGrantedAuthoritiesConverter authoritiesConverter =
+                new JwtGrantedAuthoritiesConverter();
+
+        authoritiesConverter.setAuthoritiesClaimName("roles");
+        authoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return converter;
+    }
+
+    // TODO: Very weak approach. Consider a better strategy later (RSA).
+    @Bean
+    JwtDecoder jwtDecoder(
+            @Value("${jwt.secret}") String secret
+    ) {
+        SecretKey key = new SecretKeySpec(
+                secret.getBytes(StandardCharsets.UTF_8),
+                "HmacSHA256"
+        );
+        System.out.println("CAllin decoder");
+        return NimbusJwtDecoder.withSecretKey(key).build();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin/**").hasAuthority("ADMIN")
-                        .anyRequest().permitAll());
+                        .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/actuator/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET).permitAll()
+//                        .anyRequest().permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(
+                        oauth2 ->
+                                oauth2.jwt(jwt ->
+                                        jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
+                );
         return http.build();
     }
 
