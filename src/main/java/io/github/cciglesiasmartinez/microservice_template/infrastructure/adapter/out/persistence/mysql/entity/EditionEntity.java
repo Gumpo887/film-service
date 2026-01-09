ca611fa530dd8af61619d3a07f5beff27c1e7294
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2026-01-09 21:27:34 +0100

    FEATURE: Adds Slug VO to Edition entity.

--- src/main/java/io/github/cciglesiasmartinez/microservice_template/infrastructure/adapter/out/persistence/mysql/entity/EditionEntity.java
+++ src/main/java/io/github/cciglesiasmartinez/microservice_template/infrastructure/adapter/out/persistence/mysql/entity/EditionEntity.java
@@ -31,32 +31,14 @@ public class EditionEntity implements Serializable {
     @JoinColumn(name = "film_id", nullable = false)
     private FilmEntity film;
 
-    /*
-    Cover picture
- */
+    @Column(name = "slug")
+    private String slug;
+
     @Column(name = "cover_picture")
     private String coverPicture;
 
-    /*
-        Information codes
-     */
     @Column(name = "barcode")
     private String barCode;
-
-//    @Column(name = "upc_code")
-//    private String upcCode;
-//
-//    @Column(name = "ean_code")
-//    private String eanCode;
-//
-//    @Column(name = "catalogue_number")
-//    private String catalogueNumber;
-
-    /*
-        Edition information
-     */
-//    @Column(name = "edition_name")
-//    private String editionName;
 
     @Column(name = "country")
     private String country;
@@ -65,57 +47,12 @@ public class EditionEntity implements Serializable {
     @Column(name = "format")
     private Format format;
 
-//    @Column(name = "region_code")
-//    private String regionCode;
-
-    /*
-        Release information
-     */
     @Column(name = "release_year")
     private Year releaseYear;
 
-//    @Column(name = "distributor")
-//    private String distributor;
-//
-//    @Column(name = "publisher")
-//    private String publisher;
-//
-//    @Column(name = "out_of_print")
-//    private boolean outOfPrint;
-
-    /*
-        Physical details
-     */
     @Enumerated(EnumType.STRING)
     @Column(name = "packaging_type")
     private PackagingType packagingType;
-//
-//    @Column(name = "case_dimensions")
-//    private String caseDimensions;
-
-    /*
-        Special editions information
-     */
-//    @Column(name = "is_restored")
-//    private boolean isRestoredVersion;
-//
-//    @Column(name = "is_directors_cut")
-//    private boolean isDirectorsCutVersion;
-//
-//    @Column(name = "version_type")
-//    private String versionType;
-
-    /*
-        Images and media
-     */
-//    @Column(name = "cover_image_url")
-//    private String coverImagenUrl;
-//
-//    @Column(name = "back_cover_image_url")
-//    private String backCoverImageUrl;
-//
-//    @Column(name = "disc_image_url")
-//    private String discImageUrl;
 
     @OneToMany(
             mappedBy = "edition",
@@ -124,9 +61,6 @@ public class EditionEntity implements Serializable {
     )
     private List<PictureEntity> pictures = new ArrayList<>();
 
-    /*
-        Metadata
-     */
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
@@ -138,17 +72,5 @@ public class EditionEntity implements Serializable {
 
     @Column(name = "notes")
     private String notes;
-
-    /*
-        Media
-     */
-//    @OneToMany(
-//            mappedBy = "edition",
-//            cascade = CascadeType.ALL,
-//            orphanRemoval = true
-//    )
-//    private List<MediaEntity> medias = new ArrayList<>();
-
-    // TODO: Review a refine all these fields.
 
 }
